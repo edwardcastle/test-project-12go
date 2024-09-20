8b79602a9837648e06fe4252de456a2bb22d4ace
_edwardcastle <ecastle@elkanodata.com>
2024-09-21 00:03:31 +0200

    feat: create PhoneInput.vue component

---
+++ src/components/PhoneInput.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder: string
+}>()
+</script>
+
+<template>
+  <section>
+    <input type="text" :placeholder="placeholder" />
+  </section>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
+//Vue
+import { onMounted, ref } from 'vue'
+// components
+import PhoneInput from '@/components/PhoneInput.vue'
+
+const browserLocale = ref<string>('')
+const placeholder = ref<string>('Phone Number')
+
+onMounted(() => {
+  browserLocale.value = navigator.language || navigator.userLanguage
+  if (browserLocale.value === 'en-US') {
+    placeholder.value = '+1 (123) 456-7890'
+  }
+})
 </script>
 
 <template>
-  <main>
+  <main class="container">
+    <div class="box">
+      <PhoneInput :placeholder="placeholder" />
+    </div>
   </main>
 </template>
+
+<style scoped>
+.container {
+  .box {
+    display: flex;
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
